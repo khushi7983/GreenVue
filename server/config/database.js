c69505edc044f4445f8412ab75d302a66f61e6fd
Khushi Panwar <khushipanwargzb@gmail.com>
2025-11-16 02:03:08 +0530

    refactor: update MongoDB connection URI and remove seed data

--- server/config/database.js
+++ server/config/database.js
@@ -10,7 +10,6 @@ export const connectDB = async () => {
       maxPoolSize: 10, // Maintain up to 10 socket connections
       serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
       socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-      bufferMaxEntries: 0, // Disable mongoose buffering
     };
     
     await mongoose.connect(mongoURI, options);
